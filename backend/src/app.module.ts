81dc0bf5a9e6cc1be55899d03a55ec506ca5d4b5
Daniel Correa <yo@danielgara.com>
2026-09-06 11:18:31 -0500

    imp: tutorial-6-d

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller.js';
-import { AppService } from './app.service.js';
+import { HomeModule } from './home/home.module.js';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HomeModule],
 })
 export class AppModule {}
